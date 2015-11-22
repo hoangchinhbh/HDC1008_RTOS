def104bbaf7b73dc98e24ab495010cd3f3ef5a53
Eric Bauer <eric.bauer55@gmail.com>
2015-11-22 14:40:34 -0500

    Added the initialization of the double-buffered Queue

--- hdc1008rtos.c
+++ hdc1008rtos.c
@@ -1,37 +1,10 @@
 /*
- * Copyright (c) 2015, Texas Instruments Incorporated
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * *  Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * *  Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * *  Neither the name of Texas Instruments Incorporated nor the names of
- *    its contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *    HDC1008 Logger
+ *    Written by: Eric Bauer
  */
 
 /*
- *  ======== empty_min.c ========
+ *  ======== hdc1008rtos.c ========
  */
 /* XDCtools Header files */
 #include <xdc/std.h>
@@ -49,18 +22,42 @@
 /* Board Header file */
 #include "Board.h"
 
-/*
- *  ======== heartBeatFxn ========
- *  Toggle the Board_LED0. The Task_sleep is determined by arg0 which
- *  is configured for the heartBeat Task instance.
- */
-Void heartBeatFxn(UArg arg0, UArg arg1)
+
+void readSensorBufferFxn()
 {
-    while (1) {
-        Task_sleep((UInt)arg0);
-        GPIO_toggle(Board_LED0);
-    }
+	/*
+	 *   ======== readSensorBuffer ========
+	 *
+	 */
+
+	/*  Prologue  */
+
+	/*  Loop      */
+
+	/*  Epilogue  */
 }
+
+void writeSensorBufferFxn()
+{
+	/*
+	 *   ======== writeSensorBuffer ========
+	 *
+	 */
+
+	/*  Prologue  */
+
+	/*  Loop      */
+
+	/*  Epilogue  */
+}
+
+
+typedef struct hdcMsg {
+	Queue_Elem elem;
+	int * temperatureBuffer;
+	int * humidityBuffer;
+	int bufferLength;
+} Msg;
 
 /*
  *  ======== main ========
@@ -75,6 +72,24 @@ int main(void)
 
     /* Turn on user LED  */
     GPIO_write(Board_LED0, Board_LED_ON);
+
+    /* Prime a Double-buffered Queue */
+    int msgTempBufA[10] = {0,0,0,0,0,0,0,0,0,0};
+    int msgTempBufB[10] = {0,0,0,0,0,0,0,0,0,0};
+    int msgHmdyBufA[10] = {0,0,0,0,0,0,0,0,0,0};
+    int msgHmdyBufB[10] = {0,0,0,0,0,0,0,0,0,0};
+
+    Msg msgA;
+    msgA.temperatureBuffer = msgTempBufA;
+    msgA.humidityBuffer = msgHmdyBufA;
+    msgA.bufferLength = 10;
+    Msg msgB;
+    msgB.temperatureBuffer = msgTempBufB;
+    msgB.humidityBuffer = msgHmdyBufB;
+	msgB.bufferLength = 10;
+
+    Queue_put(toReadQueue, &(msgA.elem));
+    Queue_put(toReadQueue, &(msgB.elem));
 
     /* Start BIOS */
     BIOS_start();
