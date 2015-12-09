c7c673573b3c0528a9959bff9f5464a431dc72fd
Eric Bauer <eric.bauer55@gmail.com>
2015-12-09 06:38:17 -0500

    making shit buffers global and pre-initialized lets it work...

--- hdc1008rtos.cpp
+++ hdc1008rtos.cpp
@@ -27,11 +27,12 @@
 #define BUFFER_SIZE 10
 
 /* Global Variables */
- Msg msg;
+uint16_t temperatureBuffer[BUFFER_SIZE] = {0,0,0,0,0,0,0,0,0,0};
+uint16_t humidityBuffer[BUFFER_SIZE] = {0,0,0,0,0,0,0,0,0,0};
+uint16_t dataCount = 0;
 
 /* Function Prototypes */
 // initialization functions
-void message_init();
 void i2c_config();
 void fatfs_config();
 // loop functions
@@ -49,14 +50,19 @@ int main(void)
     Board_initI2C();
     Board_initSDSPI();
     /* Call framework init functions. */
-    message_init();
+
 
     /* Turn on user LED to indicate successful init  */
     GPIO_write(Board_LED0, Board_LED_ON);
 
     while(1){
     	readSensorData();
-    	writeSensorData();
+    	dataCount++;
+    	if(dataCount>=BUFFER_SIZE){
+    		dataCount = 0;
+    		writeSensorData();
+    	}
+
     }
 
 
@@ -65,12 +71,8 @@ int main(void)
 
 void readSensorData(){
 	/* Read Data Process */
-	int i = 0;
-	for(i=0;i<BUFFER_SIZE;i++){
-		msg.humidityBuffer[i] = 0x1337; //0x66CA; // humidity = 40%
-		msg.temperatureBuffer[i] = 0xABCD; //0x60F4; // temperature = 72F
-		msg.dataCount = i+1;
-	}
+	humidityBuffer[dataCount] = 0x1337; //0x66CA; // humidity = 40%
+	temperatureBuffer[dataCount] = 0xABCD; //0x60F4; // temperature = 72F
 	/* End Data Read */
 }
 
@@ -80,25 +82,11 @@ void writeSensorData(){
 	double temp= 0.0;
 	double hmd = 0.0;
 	for(i=0;i<BUFFER_SIZE;i++){
-		temp = msg.temperatureBuffer[i];
+		temp = temperatureBuffer[i];
 		temp = (temp/65535.0)*165.0-40.0;
-		hmd = msg.humidityBuffer[i];
+		hmd = humidityBuffer[i];
 		hmd = (hmd/65535.0)*100;
 	}
 	/* End Data Write */
 }
 
-void message_init(){
-/* Primes a single-buffered Queue */
-	/* Create and initialize data buffers */
-    uint16_t msgTempBufA[BUFFER_SIZE];
-    uint16_t msgHmdyBufA[BUFFER_SIZE];
-    int i;
-    for(i=0; i<BUFFER_SIZE; i++){ msgTempBufA[i] = 1; }
-    for(i=0; i<BUFFER_SIZE; i++){ msgHmdyBufA[i] = 2; }
-
-    /* Compile the messages to store the data buffers */
-    msg.temperatureBuffer = msgTempBufA;
-    msg.humidityBuffer = msgHmdyBufA;
-    msg.dataCount = 0; // increments up to BUFFER_SIZE
-}
